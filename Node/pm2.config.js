76fdae5db17e367640737b80592eafc6ee87acb3
Jonathan Xakellis <jxakellis@gmail.com>
2022-10-22 19:37:23 -0700

    pm2 config

--- Node/pm2.config.js
+++ Node/pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'Hound PM2',
-      script: `sudo npm run houndServer`,
+      script: `sudo npm --prefix ${__dirname} run houndServer`,
       out_file: `${__dirname}/logs/out.log`,
       error_file: `${__dirname}/logs/error.log`,
       // Process is only considered running if it stays alive for more than min_uptime
