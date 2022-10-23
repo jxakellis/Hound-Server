de58c9e322119e7e04b6ff5f0306b262f147440a
Jonathan Xakellis <jxakellis@gmail.com>
2022-10-22 19:29:29 -0700

    Update cloneRepo.sh

--- Bash/cloneRepo.sh
+++ Bash/cloneRepo.sh
@@ -5,47 +5,48 @@ echo
 
 cd
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PARENT_DIR="${SCRIPT_DIR}/../../"
-NODE_DIR="${PARENT_DIR}/Hound-Server/Node/"
+PARENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+HOUND_SERVER_DIR="${PARENT_DIR}/Hound-Server/"
 
 echo
 echo "DIRECTORIES"
 echo
 
-echo "SCRIPT DIRECTORY: ${SCRIPT_DIR}"
 echo "PARENT DIRECTORY: ${PARENT_DIR}"
-echo "NODE DIRECTORY: ${NODE_DIR}"
+echo "HOUND SERVER DIRECTORY: ${HOUND_SERVER_DIR}"
 
 echo
 echo "DELETING"
 echo
 
-echo "MOVING '/NODE_DIR/main/secrets' INTO '/PARENT_DIR/'"
-mv -f "${NODE_DIR}/main/secrets/" "${PARENT_DIR}"
+echo "MOVING '/HOUND_SERVER_DIR/Node/main/secrets/' INTO '/PARENT_DIR/'"
+mv -f "${HOUND_SERVER_DIR}/Node/main/secrets/" "${PARENT_DIR}"
 
-echo "DELETING '/PARENT_DIR/Hound-Server/'"
-rm -rf "${PARENT_DIR}/Hound-Server/"
+echo "DELETING '/HOUND_SERVER_DIR/'"
+rm -rf "${HOUND_SERVER_DIR}"
 
 echo
 echo "CLONING"
 echo
 
-echo "CLONING UPDATED 'Hound-Server' FROM GitHub"
+echo "CLONING 'Hound-Server' INTO '/PARENT_DIR/'"
 git -C "${PARENT_DIR}" clone git@github.com:jxakellis/Hound-Server.git
 
-echo "GRANTING R & W PRIVILEGES ON '/PARENT_DIR/Hound-Server/'"
-chmod -R a+rwx "${PARENT_DIR}/Hound-Server/"
+echo "GRANTING R & W PRIVILEGES ON '/HOUND_SERVER_DIR/'"
+chmod -R a+rwx "${HOUND_SERVER_DIR}/"
 
-echo "INSTALLING node_modules IN /NODE_DIR/"
-npm --prefix "${NODE_DIR}" i
+echo "INSTALLING node_modules IN /HOUND_SERVER_DIR/Node/"
+npm --prefix "${HOUND_SERVER_DIR}/Node/" i
 
-echo "GRANTING R & W PRIVILEGES ON '/PARENT_DIR/Hound-Server/' (again)"
+echo "GRANTING R & W PRIVILEGES ON '/HOUND_SERVER_DIR/' (again)"
 # Grant privileges AGAIN so that jxakellis has read/write privileges on node_modules folder
-chmod -R a+rwx "${PARENT_DIR}/Hound-Server/"
+chmod -R a+rwx "${HOUND_SERVER_DIR}/"
 
-echo "MOVING '/PARENT_DIR/secrets/' INTO '/NODE_DIR/main/'"
-cp -rf "${PARENT_DIR}/secrets/" "${NODE_DIR}/main/"
+echo "MOVING '/PARENT_DIR/secrets/' INTO '/HOUND_SERVER_DIR/Node/main/'"
+mv -f "${PARENT_DIR}/secrets/" "${HOUND_SERVER_DIR}/Node/main/"
+
+echo "MOVING '/HOUND_SERVER_DIR/Bash/cloneRepo.sh' INTO '/PARENT_DIR/' "
+mv -f "${HOUND_SERVER_DIR}/Bash/cloneRepo.sh" "${PARENT_DIR}"
 
 echo
 echo "PM2"
@@ -57,8 +58,8 @@ pm2 stop all
 echo "DELETING ALL PROCESSES"
 pm2 delete all
 
-echo "STARTING '/NODE_DIR/pm2.config.js'"
-pm2 start "${NODE_DIR}/pm2.config.js"
+echo "STARTING '/PARENT_DIR/Node/pm2.config.js'"
+pm2 start "${PARENT_DIR}/Node/pm2.config.js"
 
 echo "SAVING PROCESSES"
 pm2 save --force
