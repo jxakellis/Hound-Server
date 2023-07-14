9f8b8d8f941ce3d229fb674a5b46b0e14899c936
Jonathan Xakellis <jxakellis@gmail.com>
2023-07-14 18:23:00 -0500

    reordered delete user function

--- Node/controllers/deleteFor/deleteForUser.js
+++ Node/controllers/deleteFor/deleteForUser.js
@@ -17,11 +17,19 @@ async function deleteUserForUserId(databaseConnection, userId) {
     throw new ValidationError('databaseConnection or userId missing', global.CONSTANT.ERROR.VALUE.MISSING);
   }
 
-  // We need to get the familyId for the user first before we try to delete them, otherwise the fucntion will always return null
+  // Deleting the user from our database is a multi-step process. We need to delete the user from the family first, then delete the user from the database
+  // Otherwise, the family function will malfunction because the user is missing from users
   const familyId = await getFamilyIdForUserId(databaseConnection, userId);
 
-  // We first delete the user from the database first, as this is easily reversible
-  // keep record of user being deleted, do this first so the delete statement doesn't mess with this query
+  // The user is in a family, either attempt to delete the family or have the user leave the family
+  if (areAllDefined(familyId) === true) {
+    // Since the path for delete user doesn't have familyId attached (as it predicates the family path), no active subscription is attached
+    const familyActiveSubscription = await getActiveInAppSubscriptionForFamilyId(databaseConnection, familyId);
+
+    // This step is reversible but sends a non-reversible notification at the end, so we save it for the very end so the notif is only sent if everything else is successful
+    await deleteFamilyLeaveFamilyForUserIdFamilyId(databaseConnection, userId, familyId, familyActiveSubscription);
+  }
+
   await databaseQuery(
     databaseConnection,
     `INSERT INTO previousUsers
@@ -48,15 +56,6 @@ async function deleteUserForUserId(databaseConnection, userId) {
   ];
 
   await Promise.all(promises);
-
-  // The user is in a family, either attempt to delete the family or have the user leave the family
-  if (areAllDefined(familyId) === true) {
-    // Since the path for delete user doesn't have familyId attached (as it predicates the family path), no active subscription is attached
-    const familyActiveSubscription = await getActiveInAppSubscriptionForFamilyId(databaseConnection, familyId);
-
-    // This step is reversible but sends a non-reversible notification at the end, so we save it for the very end so the notif is only sent if everything else is successful
-    await deleteFamilyLeaveFamilyForUserIdFamilyId(databaseConnection, userId, familyId, familyActiveSubscription);
-  }
 }
 
 module.exports = { deleteUserForUserId };
